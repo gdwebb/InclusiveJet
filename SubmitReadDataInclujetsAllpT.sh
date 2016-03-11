81626592e4bf54ef676051a084a9c9a9702749f8
gdwebb <gdwebb@bnl.gov>
2016-03-11 14:37:01 -0500

    Simple bash script to run the inclusive jet reader over all the data files.

---
+++ SubmitReadDataInclujetsAllpT.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+root4star -b -q /star/u/gdwebb/InclusiveJet/macros/ReadInclusiveJetTrees.C'("/gpfs01/star/pwg/gdwebb/Run9pp500/InclusiveJetAnalysis/R060/inclujetAna_*.root","/star/u/gdwebb/InclusiveJet/radius06/dataInclujetsR060.root")' 
